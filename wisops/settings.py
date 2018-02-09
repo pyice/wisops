c4893302681c1c4485d3ae481f0d9e44b29ffd26
caoxu <caoxu@adtec.com.cn>
2018-02-09 11:56:23 +0800

    更新settings.py里的数据库连接以及时区和中文设置

--- wisops/settings.py
+++ wisops/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wisops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wisops',
+        'USER': 'root',
+        'PASSWORD': '1qaz2wsx',
+        'HOST': 'mysql',
+        'PORT': ''
     }
 }
 
@@ -104,15 +108,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
